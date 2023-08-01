22129a99885858cf7c4805719046075e7e176fbb
Manuel Tello Siri <manuelTello.business@gmail.com>
2023-08-01 11:21:45 -0300

    Cambios para testing de la llamada a la API

--- src/Views/Search.vue
+++ src/Views/Search.vue
@@ -1,8 +1,6 @@
 <template>
     <h1>Search View</h1>
-    <SearchBar @InputChange="HandleSearchEmit"></SearchBar>
-    <input type="button" value="filter">
-    <button></button>
+    <SearchBar @FilterQueryEvent="HandleSearchEmit"></SearchBar>
     <div v-if="books_list.length > 0">
         <SearchList :books_list_prop="books_list"></SearchList>
     </div>
@@ -31,23 +29,21 @@ export default {
         }
     },
     methods: {
-        async HandleSearchEmit(change, page = 1) {
+        async HandleSearchEmit(change) {
             this.books_list = []
-            if (change.length >= 1) {
-                const response = await axios.get(`https://localhost:7226/api/Books/SearchByFilter?title=${change}&skip=${page}&totake=${5}`)
-                this.search = change
-                this.books_list = response.data.data
-                this.page = response.data.page
-                this.max_amount_of_pages = response.data.maxAmountOfPages
-                this.current_page = page
-                console.log(this.books_list)
-            }
+            const url = `https://localhost:7226/api/Books/SearchByFilter?q=${change}`
+            console.log("Cambio: ", url)
+            //const response = await axios.get(`https://localhost:7226/api/Books/SearchByFilter?title=${change}&skip=${page}&totake=${5}`)
+            const response = await axios.get(url)
+            /*
+            this.search = change
+            this.books_list = response.data.data
+            this.page = response.data.page
+            this.max_amount_of_pages = response.data.maxAmountOfPages
+            this.current_page = page
+            console.log(this.books_list)
+            */
         },
-        FilterList() {
-            const filtered_books_list = this.books_list.map((b) => {
-                const is_in_array = b.genres.indexOf()
-            })
-        }
     },
     components: { SearchBar, SearchList }
 }
